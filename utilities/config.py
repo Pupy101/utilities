7171a11405f0720cb4f5c45e6c47b104ad8f1468
Pupy101 <ser.porkhun41@gmail.com>
2023-04-23 16:58:22 +0300

    Add resizing image

--- utilities/config.py
+++ utilities/config.py
@@ -1,3 +1,17 @@
 import os
+from typing import Any, Callable, TypeVar
 
-RETRIES_COUNT = int(os.getenv("PY_UTILITIES_RETRIES_COUNT")) or 5  # type: ignore
+Item = TypeVar("Item")
+
+
+def load_env(env_name: str, transform: Callable[[Any], Item], default: Item) -> Item:
+    env_value = os.getenv(env_name)
+    if env_value is None:
+        return default
+    try:
+        return transform(env_value)
+    except Exception:  # pylint: disable=broad-exception-caught
+        return default
+
+
+RETRIES_COUNT = load_env("PY_UTILITIES_RETRIES_COUNT", int, 5)
--- utilities/data.py
+++ utilities/data.py
@@ -1,7 +1,31 @@
 import hashlib
-from typing import Generator, Iterable, List, TypeVar, Union
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Generator, Iterable, List, Tuple, TypeVar, Union
+
+import yaml
+from PIL import Image
 
 Item = TypeVar("Item")
+
+
+def load_yaml(file: Union[str, Path]) -> Any:
+    with open(file, "r") as fp:
+        return yaml.safe_load(fp)
+
+
+def load_json(file: Union[str, Path]) -> Any:
+    with open(file, "r") as fp:
+        return json.load(fp)
+
+
+def load_jsonl(file: Union[str, Path]) -> List[Any]:
+    data: List[Any] = []
+    with open(file, "r") as fp:
+        for line in fp:
+            data.append(json.loads(line))
+    return data
 
 
 def md5(data: Union[str, bytes]) -> str:
@@ -19,3 +43,31 @@ def chunking(items: Iterable[Item], chunk_size: int) -> Generator[List[Item], No
             chunk = []
     if chunk:
         yield chunk
+
+
+@dataclass
+class ResizeImage:
+    path: Union[str, Path]
+    size: int
+    check: bool = True
+
+
+def resize_image(item: ResizeImage) -> Tuple[Path, bool]:
+    path = Path(item.path)
+    if not path.exists():
+        return path, False
+    image = Image.open(path)
+    width, height, channels = image.width, image.height, len(image.mode)
+    if item.check and channels != 3:
+        try:
+            path.unlink()
+        except Exception:  # pylint: disable=broad-exception-caught
+            return path, False
+    if width <= height:
+        width, height = item.size, round(item.size * height / width)
+    else:
+        width, height = round(item.size * width / height), item.size
+    shape = (width, height)
+    resized_image = image.resize(shape, Image.ANTIALIAS)
+    resized_image.save(path)
+    return path, True
